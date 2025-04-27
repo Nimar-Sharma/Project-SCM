bead3f9a693b45e448e5f6b55bc8b65c77e55944
laksh <lakshaybisht77@gmail.com>
2025-04-27 21:43:44 +0530

    Update idk.js
    starting on idk.js part

---
+++ idk.js
@@ -0,0 +1,19 @@
+(function() {
+    "use strict";
+  
+    const responses = {
+      greetings: [
+        "Hello there! 👋",
+        "Hi! How can I assist you? 🤖",
+        "Hey! What's up? 🚀",
+        "Greetings, human! 👽",
+        "Yo! 😎"
+      ],
+      default: [
+        "I'm thinking... 🤔",
+        "Interesting!",
+        "I don't know about that. 🤷",
+        "I'm still processing that... 🧠",
+        "Access denied! 🚫",
+      ]
+    };
